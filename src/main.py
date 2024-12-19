8107748fb8ba4ddf6ed901bc406962a51ecbf6ad
Server <chauanphu@gmail.com>
2024-12-19 17:21:41 +0700

    lint

--- src/main.py
+++ src/main.py
@@ -6,24 +6,8 @@ from utils.config import POPULATION_SIZE, MAX_ITER, ALLOW_EARLY
 logging.basicConfig(level=logging.INFO)
 import networkx as nx
 
-# def sa_main():
-#     # Load the Solomon VRP benchmark file
-#     vehicle_info, customer_data = load_solomon_vrp('benchmark/C101.txt')
-#     print("Vehicle Info:", vehicle_info.__dict__)
-#     # Convert to orders
-#     orders = convert_to_orders(customer_data)
-#     print(f"Number of orders: {len(orders)}")
-#     end_depot = Location(0, customer_data[0].xcoord, customer_data[0].ycoord)
-#     initial_solution: SASolution = init_solution(orders, end_depot, vehicle_info.capacity)
-#     print("Initial Solution:", initial_solution.total_distance())
-#     solver = SimulatedAnnealingSolver(initial_solution, initial_temp=1000, cooling_rate=0.95, stopping_temp=1)
-#     best_solution, history = solver.anneal()
-#     # Save history to a JSON file
-#     with open('output/history.json', 'w') as f:
-#         json.dump(history, f)
-
 def pso_main():
-    orders, locations, vehicles  = load_voratas_vrp('benchmark/mdpdr/Bac1.txt', 20)
+    orders, locations, vehicles  = load_voratas_vrp('Aar1')
     print("Number of orders:", len(orders))
     print("Number of locations:", len(locations))
     print("Number of vehicles:", len(vehicles))
