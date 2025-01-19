c9cc2b51f31b78f2dc78b48f9c3bf2e412a9cf4a
Server <chauanphu@gmail.com>
2025-01-19 10:13:13 +0700

    feat: update code

--- src/main.py
+++ src/main.py
@@ -1,22 +1,25 @@
+from meta.PSO import MultiSwarmPSO
 from utils import load_data
 from utils.config import DATA_PATH
 
 loader = load_data.VRPDataLoader(DATA_PATH)
 loader.load_data()
 
-print("Number of Customers:", loader.get_num_customers())
-print("Number of Lockers:", loader.get_num_lockers())
-print("Number of Vehicles:", loader.get_num_vehicles())
-print("Vehicle Capacity:", loader.get_capacity())
-print("Demands:", loader.get_demands())
-for data in loader.get_customer_data():
-    print("\t", data)
-print("Depot Data:", loader.get_depot_data())
-print("Locker Data:", loader.get_locker_data())
-print("Locker Assignments:", loader.get_locker_assignments())
-
-# Display the distance matrix
-distance_matrix = loader.get_distance_matrix()
-print("Distance Matrix:")
-for row in distance_matrix:
-    print("\t", row)
+# Customer data
+customer_data = loader.get_customer_data()
+# Vehicle data
+vehicle_data = loader.get_num_vehicles()
+# Depot data
+depot_data = loader.get_depot_data()
+print("Customer data")
+for customer in customer_data:
+    print(customer)
+print("Vehicle data")
+print(vehicle_data)
+print("Depot data")
+for depot in depot_data:
+    print(depot)
+# Print parcel locker data
+parcel_locker_data = loader.locker_data
+for locker in parcel_locker_data:
+    print(locker)
